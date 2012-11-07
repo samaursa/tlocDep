98a0ddbea24549470e5ddb573f87c1f5f9857a1b
Matiaus <Matiaus@localhost>
2012-11-07 06:01:16 -0500

    [FIX][REN] x64 configuration now builds x64 configurations per project|Renamed multiple configurations to match tlocEngine - DebugStdAlloc configuration was renamed to Debug_StdAlloc for Box2D - All references of the Release_DebugInfo_StdAlloc configuration were renamed   to Release_DebugInfo_StdAlloc to match tlocEngine

--- src/tlocDepVersion.h
+++ src/tlocDepVersion.h
@@ -12,7 +12,7 @@
 // so the local-file will then have: x_x_6_12346
 // (please see end-of-file for rationalle behind this)
 
-#ifndef TLOC_DEP_VERSION_0_0_47_1352284430
+#ifndef TLOC_DEP_VERSION_0_0_50_1352285701
 #  error "You are building against an incorrect/older version of the library. The correct version is the macro above."
 #endif
 
