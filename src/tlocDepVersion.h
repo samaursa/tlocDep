75e45100fff053e88fcc489845de2bca65b03b51
Matiaus <Matiaus@localhost>
2012-11-07 04:32:49 -0500

    [INCR][ADD] Added a Box2D project in order to build Box2D into a library|Builds debug and release, missing other configurations

--- src/tlocDepVersion.h
+++ src/tlocDepVersion.h
@@ -12,7 +12,7 @@
 // so the local-file will then have: x_x_6_12346
 // (please see end-of-file for rationalle behind this)
 
-#ifndef TLOC_DEP_VERSION_0_0_39_1351789718
+#ifndef TLOC_DEP_VERSION_0_0_42_1352280285
 #  error "You are building against an incorrect/older version of the library. The correct version is the macro above."
 #endif
 
