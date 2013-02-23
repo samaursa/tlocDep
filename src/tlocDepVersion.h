30905d055bee689b994982144f3e7643b6c2ddc9
Matiaus <neil.j.koo@gmail.com>
2013-02-23 11:19:15 -0500

    [MOD][ADD] Modified build path for Xcode project | Added a release build configuration for Xcode project

--- src/tlocDepVersion.h
+++ src/tlocDepVersion.h
@@ -12,7 +12,7 @@
 // so the local-file will then have: x_x_6_12346
 // (please see end-of-file for rationalle behind this)
 
-#ifndef TLOC_DEP_VERSION_0_0_73_1360327737
+#ifndef TLOC_DEP_VERSION_0_0_75_1361635787
 #  error "You are building against an incorrect/older version of the library. The correct version is the macro above."
 #endif
 
