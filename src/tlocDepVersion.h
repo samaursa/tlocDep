19cacf3544efa4cb3eadc557db36fab97a53fc03
samaursa <samaursa@localhost>
2013-03-25 00:40:57 -0400

    [MIN] No major changes

--- src/tlocDepVersion.h
+++ src/tlocDepVersion.h
@@ -12,7 +12,7 @@
 // so the local-file will then have: x_x_6_12346
 // (please see end-of-file for rationalle behind this)
 
-#ifndef TLOC_DEP_VERSION_0_0_80_1364091286
+#ifndef TLOC_DEP_VERSION_0_0_82_1364186440
 #  error "You are building against an incorrect/older version of the library. The correct version is the macro above."
 #endif
 
