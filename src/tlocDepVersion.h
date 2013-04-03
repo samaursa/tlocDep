645cd02c5ff3a6845e0e34af666daa90993bc7ad
samaursa <samaursa@localhost>
2013-04-02 21:36:19 -0400

    [INCR] Worked around the extraction (bug in CMake? see below) of tarball by putting the tar in WinSDK folder | Extraction and copying works (perhaps we should use INSTALL) - CMake appears to have a bug where I cannot extract the tarball into any   folder other than the current folder.

--- src/tlocDepVersion.h
+++ src/tlocDepVersion.h
@@ -12,7 +12,7 @@
 // so the local-file will then have: x_x_6_12346
 // (please see end-of-file for rationalle behind this)
 
-#ifndef TLOC_DEP_VERSION_0_0_87_1364850692
+#ifndef TLOC_DEP_VERSION_0_0_88_1364850693
 #  error "You are building against an incorrect/older version of the library. The correct version is the macro above."
 #endif
 
