31fda30d5e2527e1a446c86d97bd9b9d2b1ca176
samaursa <samaursa@localhost>
2013-04-07 00:05:48 -0400

    [MIN] Removed TLOC_CONFIG_... because setting those is NOT possible with CMake - Any configuration related flags must be set via CMAKE_CXX_FLAGS_...

--- src/tlocDepVersion.h
+++ src/tlocDepVersion.h
@@ -12,7 +12,7 @@
 // so the local-file will then have: x_x_6_12346
 // (please see end-of-file for rationalle behind this)
 
-#ifndef TLOC_DEP_VERSION_0_0_129_1365092543
+#ifndef TLOC_DEP_VERSION_0_0_130_1365307463
 #  error "You are building against an incorrect/older version of the library. The correct version is the macro above."
 #endif
 
