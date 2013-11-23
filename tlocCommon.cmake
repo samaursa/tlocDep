d272293395178415cd56fe68fe3049e8b8877a5d
samaursa <saadrustam@gmail.com>
2013-11-23 17:45:36 -0500

    [MOD] Compiler flags added to tlocCommon.cmake as a function (TESTED in tlocDep)

--- tlocCommon.cmake
+++ tlocCommon.cmake
@@ -33,7 +33,6 @@ if(MSVC)
   message(STATUS "COMPILER: MSVC")
   set(TLOC_COMPILER_MSVC 1)
   if(MSVC90 OR MSVC80 OR MSVC71 OR MSVC70 OR MSVC60)
-    message(STATUS "C++11: Not supported")
     set(TLOC_COMPILER_C11 0)
   endif()
 elseif(CMAKE_COMPILER_IS_GNUCC OR CMAKE_COMPILER_IS_GNUCXX)
@@ -49,6 +48,115 @@ else()
   message("COMPILER: Unsupported!")
   set(TLOC_COMPILER_UNSUPPORTED 1)
 endif()
+
+if (TLOC_COMPILER_C11)
+  message(STATUS "C++11: Supported")
+else()
+  message(STATUS "C++11: Not supported")
+endif()
+
+#------------------------------------------------------------------------------
+# Compiler flags
+
+# ---------- Remove MINSIZEREL ----------
+set(CMAKE_CONFIGURATION_TYPES Debug Release RelWithDebInfo CACHE TYPE INTERNAL FORCE )
+
+# ---------- Backup the default flags ----------
+set(TLOC_CXX_FLAGS_DEFAULT                  ${CMAKE_CXX_FLAGS})
+set(TLOC_CXX_FLAGS_DEBUG_DEFAULT            ${CMAKE_CXX_FLAGS_DEBUG})
+set(TLOC_CXX_FLAGS_RELEASE_DEFAULT          ${CMAKE_CXX_FLAGS_RELEASE})
+set(TLOC_CXX_FLAGS_RELWITHDEBINFO_DEFAULT   ${CMAKE_CXX_FLAGS_RELWITHDEBINFO})
+
+# default definitions
+function(tloc_revert_definitions)
+  set(CMAKE_CXX_FLAGS_DEBUG          ${CMAKE_CXX_FLAGS_DEBUG_DEFAULT} PARENT_SCOPE)
+  set(CMAKE_CXX_FLAGS_RELEASE        ${CMAKE_CXX_FLAGS_RELEASE_DEFAULT} PARENT_SCOPE)
+  set(CMAKE_CXX_FLAGS_RELWITHDEBINFO ${CMAKE_CXX_FLAGS_RELWITHDEBINFO_DEFAULT} PARENT_SCOPE)
+endfunction()
+
+# common definitions
+function(tloc_add_common_definitions)
+  # C++03 is currently supported (support will be dropped soon)
+  if (NOT TLOC_COMPILER_C11)
+    add_definitions(-DTLOC_CXX03)
+  endif()
+
+  # visual studio compiler flags
+  if (TLOC_COMPILER_MSVC)
+    add_definitions(-D_CRT_SECURE_NO_WARNINGS)
+  endif()
+endfunction()
+
+#------------------------------------------------------------------------------
+# Strict compiler flags required by the engine. This disables RTTI and 
+# Exceptions handling
+function(tloc_add_definitions_strict)
+  tloc_revert_definitions()
+  tloc_add_common_definitions()
+
+  # visual studio compiler and linker flags
+  if (TLOC_COMPILER_MSVC)
+    set(CMAKE_CXX_FLAGS_DEBUG           "-DTLOC_DEBUG /Od /Gm /RTC1 /MTd /GR- /W4 /WX /c /Zi /TP" PARENT_SCOPE)
+    set(CMAKE_CXX_FLAGS_RELEASE         "-DTLOC_RELEASE /O2 /Ob2 /Oi /Ot /GL /MT /Gy /GR- /W4 /WX /c /Zi /TP" PARENT_SCOPE)
+    set(CMAKE_CXX_FLAGS_RELWITHDEBINFO  "-DTLOC_RELEASE_DEBUGINFO /O2 /Ob2 /Oi /Ot /Gm /MT /Gy /GR- /W4 /WX /c /Zi /TP" PARENT_SCOPE)
+    set(CMAKE_EXE_LINKER_FLAGS_RELEASE  "${CMAKE_EXE_LINKER_FLAGS_RELEASE} /LTCG")
+
+    #turn off exceptions for all configurations
+    string(REGEX REPLACE "/EHsc" "" CMAKE_CXX_FLAGS ${CMAKE_CXX_FLAGS} )
+    string(REGEX REPLACE "/GX" "" CMAKE_CXX_FLAGS ${CMAKE_CXX_FLAGS} )
+
+    # cmake bug (adds /Zm1000 when it is not needed): http://public.kitware.com/Bug/view.php?id=13867
+    string(REGEX REPLACE "/Zm1000" "" CMAKE_CXX_FLAGS ${CMAKE_CXX_FLAGS} )
+
+    # The above operations are local, make them parent scope
+    set(CMAKE_CXX_FLAGS ${CMAKE_CXX_FLAGS} PARENT_SCOPE)
+
+  elseif (TLOC_COMPILER_XCODE) # xcode compiler and linker flags
+    # Note that -gdwarf-2 generates debug symbols (dsym files in dwarf standard)
+    set(CMAKE_CXX_FLAGS_DEBUG "${CMAKE_CXX_FLAGS_DEBUG} -DTLOC_DEBUG -std=c++11 -stdlib=libc++ -fno-exceptions -fno-rtti -Wno-unused-function -g -Wall -Wno-long-long -pedantic" PARENT_SCOPE)
+    set(CMAKE_CXX_FLAGS_RELEASE "${CMAKE_CXX_FLAGS_RELEASE} -DTLOC_RELEASE -std=c++11 -stdlib=libc++ -fno-exceptions -fno-rtti -Wno-unused-function -Wall -Wno-long-long -pedantic" PARENT_SCOPE)
+    set(CMAKE_CXX_FLAGS_RELWITHDEBINFO "${CMAKE_CXX_FLAGS_RELWITHDEBINFO} -std=c++11 -stdlib=libc++ -DTLOC_RELEASE_DEBUGINFO -fno-exceptions -fno-rtti -Wno-unused-function -g -Wall -Wno-long-long -pedantic" PARENT_SCOPE)
+    set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -std=c++11 -stdlib=libc++ -framework Foundation -framework OpenGLES -framework AudioToolbox -framework CoreGraphics -framework QuartzCore -framework UIKit -framework OpenAL -Wall -Wno-long-long -pedantic" PARENT_SCOPE)
+  else()
+    message(WARNING "Unsupported compiler being used. Default compiler settings will be applied")
+  endif()
+endfunction()
+
+#------------------------------------------------------------------------------
+# Loose compiler flags required by projects working with libraries that require
+# RTTI and/or Exceptions to be enabled. 
+function(tloc_add_definitions)
+  tloc_revert_definitions()
+  tloc_add_common_definitions()
+
+  # non-strict definictions includes exceptions, rtti and stl
+  add_definitions(
+    -DTLOC_ENABLE_CPPUNWIND 
+    -DTLOC_ENABLE_CPPRTTI 
+    -DTLOC_USING_STL 
+    )
+
+  if(TLOC_COMPILER_MSVC)
+    set(CMAKE_CXX_FLAGS_DEBUG           "/DTLOC_DEBUG /Od /Gm  /EHsc /RTC1 /MTd /GR /W4 /WX /c /Zi /TP" PARENT_SCOPE)
+    set(CMAKE_CXX_FLAGS_RELEASE         "/DTLOC_RELEASE /O2 /Ob2 /Oi /Ot /GL /EHsc /MT /Gy /GR /W4 /WX /c /Zi /TP" PARENT_SCOPE)
+    set(CMAKE_CXX_FLAGS_RELWITHDEBINFO  "/DTLOC_RELEASE_DEBUGINFO /O2 /Ob2 /Oi /Ot /Gm /EHsc /MT /Gy /GR /W4 /WX /c /Zi /TP" PARENT_SCOPE)
+
+    #turn off exceptions for all configurations
+    string(REGEX REPLACE "/Zm1000" "" CMAKE_CXX_FLAGS ${CMAKE_CXX_FLAGS} )
+
+    # The above operations are local, make them parent scope
+    set(CMAKE_CXX_FLAGS ${CMAKE_CXX_FLAGS} PARENT_SCOPE)
+
+  elseif(TLOC_COMPILER_XCODE)
+    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -x objective-c++")
+    set(CMAKE_CXX_FLAGS_DEBUG "${CMAKE_CXX_FLAGS_DEBUG} -DTLOC_DEBUG -std=c++11 -stdlib=libc++ -fno-rtti -Wno-unused-function -g" PARENT_SCOPE)
+    set(CMAKE_CXX_FLAGS_RELEASE "${CMAKE_CXX_FLAGS_RELEASE} -DTLOC_RELEASE -std=c++11 -stdlib=libc++ -fno-rtti -Wno-unused-function" PARENT_SCOPE)
+    set(CMAKE_CXX_FLAGS_RELWITHDEBINFO "${CMAKE_CXX_FLAGS_RELWITHDEBINFO} -std=c++11 -stdlib=libc++ -DTLOC_RELEASE_DEBUGINFO -fno-rtti -Wno-unused-function -g" PARENT_SCOPE)
+  else()
+    message("Configuration not applied because compiler is unsupported (or not detected properly)")
+  endif()
+
+endfunction()
 
 #------------------------------------------------------------------------------
 # Application type
