82848de5ad89304515ee90f25f0e2d1628134540
samaursa <saadrustam@gmail.com>
2014-04-23 20:42:01 -0400

    [FIX] Fix for RapidXML print functionality where ADL (argument dependant lookup) fails for template functions in rapidxml_print.hpp - NOTES: Although that?s what the mailing list reports, I am not aware that ADL          applies to functions that the compiler doesn?t already know exist          (hence the ?fix? putting declarations of the functions before their           definitions)

--- src/tlocDepVersion.h
+++ src/tlocDepVersion.h
@@ -12,7 +12,7 @@
 // so the local-file will then have: x_x_6_12346
 // (please see end-of-file for rationalle behind this)
 
-#ifndef TLOC_DEP_VERSION_0_0_156_1397159510
+#ifndef TLOC_DEP_VERSION_0_0_157_1397159511
 #  error "You are building against an incorrect/older version of the library. The correct version is the macro above."
 #endif
 
