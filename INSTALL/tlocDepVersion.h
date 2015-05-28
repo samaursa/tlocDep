ab296deb1e3cd4d6eaf937e4ffb6ce0b8d5e5420
samaursa <saadrustam@gmail.com>
2015-05-28 12:52:30 -0400

    [MIN] Added CMake option to enable/disable "treat warnings as errors" compiler option

--- INSTALL/tlocDepVersion.h
+++ INSTALL/tlocDepVersion.h
@@ -12,7 +12,7 @@
 // so the local-file will then have: x_x_6_12346
 // (please see end-of-file for rationalle behind this)
 
-#ifndef TLOC_DEP_VERSION_0_0_204_1420164684
+#ifndef TLOC_DEP_VERSION_0_0_207_1432831918
 #  error "You are building against an incorrect/older version of the library. The correct version is the macro above."
 #endif
 
